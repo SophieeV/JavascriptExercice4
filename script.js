d26b888101720e45a673765500737a014d79628f
SophieeV <sophievesin@wanadoo.fr>
2021-03-30 14:04:59 +0200

    sauvegarde 14h00

---
+++ script.js
@@ -0,0 +1,6 @@
+function displayInput(){
+  var inputName = document.getElementById('name').value;
+  var inputSurname = document.getElementById('surname').value;
+  var inputTown = document.getElementById('town').value;
+  alert('prénom: ' + inputName + '\n' + 'nom: ' + inputSurname + '\n' + 'ville: ' + inputTown);
+}
